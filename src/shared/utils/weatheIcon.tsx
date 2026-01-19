98d1b504e19502e155f87bf9502c1b6ab5d2ba5e
경아 <ukyunga0.1@gmail.com>
2026-01-19 17:23:48 +0900

    refactor: WeatherIcon 컴포넌트 함수 내부로 이동
    - getWeatherIconUrl을 컴포넌트 내부 함수로 변경

--- src/shared/utils/weatheIcon.tsx
+++ src/shared/utils/weatheIcon.tsx
@@ -1,17 +1,6 @@
-// 아이콘 코드 이미지 url 변환
-export const getWeatherIconUrl = (iconCode: string): string => {
-    return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-};
-
-// 날씨 아이콘 컴포넌트
+// WeatherIcon.tsx
 export const WeatherIcon = ({ iconCode, size = 80 }: { iconCode: string; size?: number }) => {
-    return (
-        <img
-            src={getWeatherIconUrl(iconCode)}
-            alt="날씨 아이콘"
-            width={size}
-            height={size}
-            className="drop-shadow-lg"
-        />
-    );
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    return <img src={iconUrl} alt="날씨 아이콘" width={size} height={size} className="drop-shadow-lg" />;
 };
